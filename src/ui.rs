732f45b2b6b8aa03eb5657430ac97c9e159103ae
abdi-29 <abdoulayeba29@gmail.com>
2025-08-22 16:35:38 +0200

    fixed the board

--- src/ui.rs
+++ src/ui.rs
@@ -11,35 +11,33 @@ pub struct GomokuApp {
 impl eframe::App for GomokuApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            // ui.heading("Gomoku");
+            let cell_size = 20.0;
 
-            let cell_size = 19.0;
+            egui::Grid::new("gomoku_board")
+                .spacing([1.0, 6.0])
+                .show(ui, |ui| {
+                    for y in 0..19 {
+                        for x in 0..19 {
+                            let color = match self.board[y][x] {
+                                Some(true) => egui::Color32::BLACK,
+                                Some(false) => egui::Color32::WHITE,
+                                None => egui::Color32::RED
+                            };
 
-            // Draw the board
-            for y in 0..19 {
-                for x in 0..19 {
-                    let (rect, response) = ui.allocate_exact_size(
-                        egui::vec2(cell_size, cell_size),
-                        egui::Sense::click(),
-                    );
+                            let response = ui.add_sized(
+                                [cell_size, cell_size],
+                                egui::Button::new("").fill(color),
+                            );
 
-                    // Background
-                    let color = match self.board[y][x] {
-                        Some(true) => egui::Color32::BLACK,
-                        Some(false) => egui::Color32::WHITE,
-                        None => egui::Color32::RED,
-                    };
+                            if response.clicked() && self.board[y][x].is_none() {
+                                self.board[y][x] = Some(self.current_player);
+                                self.current_player = !self.current_player;
+                            }
 
-                    ui.painter().rect_filled(rect, 2.0, color);
-                    ui.painter().rect_stroke(rect, 12.0, egui::Stroke::new(1.0, egui::Color32::RED), StrokeKind::Outside);
-
-                    if response.clicked() && self.board[y][x].is_none() {
-                        self.board[y][x] = Some(self.current_player);
-                        self.current_player = !self.current_player;
+                        }
+                        ui.end_row();
                     }
-                }
-                ui.end_row();
-            }
+                });
         });
     }
 }
